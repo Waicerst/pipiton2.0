9f486c78b86f19b117cb6423af9c2340787b01e1
Waicerst <133808660+Waicerst@users.noreply.github.com>
2023-07-07 23:23:53 +0200

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -1,31 +1,31 @@
 import csv
 
 def read_data_from_csv(file_path):
-    x = []
-    y = []
-    z = []
+        x = []
+        y = []
+        z = []
 
-    with open(file_path, 'r') as file:
-        reader = csv.reader(file, delimiter=';')
-        next(reader)  # Skip the header row
-        next(reader)  # Skip the header row
-        for row in reader:
-            x.append(row[0])    #Country
-            y.append(row[1])    #Area
-            z.append(int(row[37].strip() if row[37].strip()!='' else 0))   #Population
+        with open(file_path, 'r') as file:
+            reader = csv.reader(file, delimiter=';')
+            next(reader)  # Skip the header row
+            next(reader)  # Skip the header row
+            for row in reader:
+                x.append(row[0])  
+                y.append(row[2])  
+                z.append(int(row[4].strip() if row[4].strip() != '' else 0))  # Population
 
-    return x,y,z
+        return x, y, z
 
-x, y, z = read_data_from_csv('factbook.csv')
+    x, y, z = read_data_from_csv('film.csv')
 
-filtered_idx = list(filter(lambda i: z[i]>1000, range(len(z))))
+    filtered_idx = list(filter(lambda i: z[i] == 'Cage, Nicolas', range(len(z))))
 
-x_filtered = [x[i] for i in filtered_idx]
-y_filtered = [y[i] for i in filtered_idx]
+    x_filtered = [x[i] for i in filtered_idx]
+    y_filtered = [y[i] for i in filtered_idx]
 
-print(x)
-print(y)
-print(z)
-print(filtered_idx)
-print(x_filtered)
-print(y_filtered)
+    print(x)
+    print(y)
+    print(z)
+    print(filtered_idx)
+    print(x_filtered)
+    print(y_filtered)
